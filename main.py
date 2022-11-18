189cd16e7d4820a2e560b8ea8ec123237507a043
Abhay Kumar Ray <abhayray2002@gmail.com>
2022-11-18 12:23:46 +0530

    followers/following logic implemented

--- main.py
+++ main.py
@@ -9,13 +9,16 @@ client = pymongo.MongoClient(
     "mongodb+srv://Abhay:Abhay123@cluster0.bba05gv.mongodb.net/?retryWrites=true&w=majority", tlsCAFile=certifi.where())
 db = client.users
 
-# db.user_instances.create_index("inserted", expireAfterSeconds = 120)
+db.register_instances.create_index("inserted", expireAfterSeconds=120)
+db.follow_instances.create_index("inserted", expireAfterSeconds=120)
 
 bot = commands.Bot(command_prefix="!", intents=discord.Intents.all())
+
 
 @bot.event
 async def on_ready():
     print("Bot Is Online")
+
 
 @bot.command()
 async def register(ctx, arg1):
@@ -23,12 +26,13 @@ async def register(ctx, arg1):
     if err != "":
         await ctx.reply(f"Username is invalid! {round(bot.latency * 1000)}ms")
     else:
-        db.user_instances.insert_one({
+        db.register_instances.insert_one({
             "username": arg1,
             "twitterId": res,
             "serverId": ctx.guild.id,
             "inserted": datetime.utcnow()})
         await ctx.reply(f"tweet something!")
+
 
 @bot.command()
 async def verify(ctx, arg1):
@@ -36,13 +40,13 @@ async def verify(ctx, arg1):
     print(arg1)
     print(serverId)
     try:
-        user_instance = db.user_instances.find_one({"serverId" : serverId})
+        user_instance = db.register_instances.find_one({"serverId": serverId})
         lastTweetContent = last_tweet(user_instance["twitterId"])
         print(lastTweetContent)
-        if lastTweetContent!=arg1:
+        if lastTweetContent != arg1:
             await ctx.reply(f"Content didn't match with your last tweet!")
         else:
-            try: 
+            try:
                 db.users.insert_one({
                     "username": user_instance["username"],
                     "twitterId": user_instance["twitterId"],
@@ -54,6 +58,7 @@ async def verify(ctx, arg1):
     except Exception as e:
         print(e)
         await ctx.reply(f"User instance not found in database!")
+
 
 @bot.command()
 async def leaderboard(ctx, arg1):
@@ -71,15 +76,56 @@ async def leaderboard(ctx, arg1):
         await ctx.reply("Entered number is more than the users present in database.")
         arg1 = len(scores)
 
-    em = discord.Embed(title = f"Top {arg1} Richest People", description = "This is based on the number of likes on twitter", color = discord.Color(0xfa43ee))
+    em = discord.Embed(title=f"Top {arg1} Richest People",
+                       description="This is based on the number of likes on twitter", color=discord.Color(0xfa43ee))
     index = 1
     for score in scores:
-        em.add_field(name = f"{score['username']}", value = f"likes: {score['likes']}", inline = False)
+        em.add_field(name=f"{score['username']}",
+                     value=f"likes: {score['likes']}", inline=False)
         if index == int(arg1):
             break
         else:
             index += 1
-    await ctx.send(embed = em)
+    await ctx.send(embed=em)
+
+
+@bot.command()
+async def follow(ctx, arg):
+    try:
+        userRequesting = db.users.find_one({"serverId": ctx.guild.id})
+        try:
+            userRequested = db.users.find_one({"username": arg})
+            channel = ctx.guild.system_channel
+            db.follow_instances.insert_one({
+                "requestingUserTwitterId": userRequesting["twitterId"],
+                "requestedUserTwitterId": userRequested["twitterId"],
+            })
+            await channel.send(f"{arg.mention} Follow {userRequesting['username']} to earn 50 coins.")
+        except:
+            await ctx.reply("User you are requesting is not registered")
+    except:
+        await ctx.reply("You are not registered")
+
+@bot.command()
+async def followed(ctx, arg):
+    try:
+        userFollowing = db.users.find_one({"serverId": ctx.guild.id})
+        try:
+            userFollowed = db.users.find_one({"username": arg})
+            request = db.request_instances.find_one({
+                "$and" : [
+                    {"requestingUserTwitterId": userFollowed["twitterId"]},
+                    {"requestedUserTwitterId": userFollowed["twitterId"]}
+                ]
+            })
+        except:
+            await ctx.reply("User you are following is not registered")
+    except:
+        await ctx.reply("You are not registered")
+
+
+
+
 
 # @bot.event
 # async def on_message(message):
