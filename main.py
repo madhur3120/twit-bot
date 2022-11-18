509f66873fca819d4ac795b0492a638399269a30
Abhay Kumar Ray <abhayray2002@gmail.com>
2022-11-18 10:26:43 +0530

    Added verification and leaderboard

--- main.py
+++ main.py
@@ -1,12 +1,15 @@
 import discord
 from discord.ext import commands
 from tweepy_setup import *
-import tweepy
 import pymongo
-from datetime import datetime, timedelta
+from datetime import datetime
+import certifi
 
-client = pymongo.MongoClient("mongodb+srv://Abhay:Abhay123@cluster0.bba05gv.mongodb.net/?retryWrites=true&w=majority")
+client = pymongo.MongoClient(
+    "mongodb+srv://Abhay:Abhay123@cluster0.bba05gv.mongodb.net/?retryWrites=true&w=majority", tlsCAFile=certifi.where())
 db = client.users
+
+# db.user_instances.create_index("inserted", expireAfterSeconds = 120)
 
 bot = commands.Bot(command_prefix="!", intents=discord.Intents.all())
 
@@ -17,45 +20,54 @@ async def on_ready():
 @bot.command()
 async def register(ctx, arg1):
     res, err = get_user_id(arg1)
-    if err!="" :
+    if err != "":
         await ctx.reply(f"Username is invalid! {round(bot.latency * 1000)}ms")
     else:
-        db.user_instance.insert_one({
-            "username" : arg1,
-            "serverId": ctx.guild.id,
-            "end_time": datetime.today() + timedelta(seconds=15),
-        })
-        # db.users.insert_one({
-        #     "username" : arg1,
-        #     "twitterId": res,
-        #     "serverId": ctx.guild.id
-        # })
-        # # await ctx.reply(f"Pong! {round(bot.latency * 1000)}ms")
-        # await ctx.reply(f"User Registered {round(bot.latency * 1000)}ms")
+        db.user_instances.insert_one({
+            "username": arg1,
+            "twitterId": res,
+            "inserted": datetime.utcnow()})
+        await ctx.reply(f"tweet something!")
 
 @bot.command()
-async def leaderboard(ctx):
+async def verify(ctx, arg1):
+    serverId = ctx.guild.id
+    try:
+        user_instance = await  db.user_instances.find_one({"serverId" : serverId})
+        lastTweetContent = last_tweet(user_instance.twitterId)
+        if lastTweetContent!=arg1:
+            await ctx.reply(f"Content didn't match with your last tweet!")
+        else:
+            db.users.insert_one({
+                "username": user_instance.username,
+                "twitterId": user_instance.twitterId,
+                "serverId": ctx.guild.id,
+            })
+    except:
+        await ctx.reply(f"User instance not found in database!")
+
+@bot.command()
+async def leaderboard(ctx, arg1):
     def cmp(ele):
         return ele['likes']
     users = db.users.find()
     scores = []
     for user in users:
         print(user["username"])
-        scores.append({"username": user["username"], "likes": likes_count(user["twitterId"])})
+        scores.append(
+            {"username": user["username"], "likes": likes_count(user["twitterId"])})
     scores.sort(key=cmp, reverse=True)
     print(scores)
-    await ctx.reply(f"Leaderboard")
-# @bot.command()
-# async def twit(ctx):
-#     r = requests.get("https://api.twitter.com/2/users/:id/tweets")
-#     res = r.json()
-#     em = discord.Embed()
-#     em.set_image(url=res['message'])
-#     await ctx.send(embed=em)
 
-
-
-
+    em = discord.Embed(title = f"Top {arg1} Richest People", description = "This is based on the number of likes on twitter", color = discord.Color(0xfa43ee))
+    index = 1
+    for score in scores:
+        em.add_field(name = f"{score.username}", likes = f"{score.likes}", inline = False)
+        if index == arg1:
+            break
+        else:
+            index += 1
+    await ctx.send(embed = em)
 
 # @bot.event
 # async def on_message(message):
@@ -70,10 +82,6 @@ async def leaderboard(ctx):
 # @bot.event
 # async def on_member_remove(member):
 #     channel = member.guild.system_channel
-#     await channel.send(f"GoodBye {member.mention}")   
-
-
-
-
+#     await channel.send(f"GoodBye {member.mention}")
 
 bot.run("MTA0MjUyOTM5MTMyODEwNDQ4OA.GLtS6q.DWTFb_GoezczLuERxTgxRs5E62SnJX7EJeyKAs")
